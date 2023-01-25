04066e8b177d6ce3fcebbc9bb3240331b1e36628
Ricky Esparza <RickyEsparza@Rickys-MacBook.local>
2023-01-25 16:21:09 -0700

    added code to Footer.js, and Footer.css also added a new folder to hold images

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+// Will render the footer of the page
+
+import React from "react";
+import '../styles/Footer.css';
+
+import GitHub from '../images/Footer/GitHub.png';
+import linkedIn from '../images/Footer/linkedIn.png';
+
+
+function Footer({ }) {
+    return (
+        <div className="setFooter">
+            <a target ='_blank' href="https://github.com/Ricky22M"><img className="setMargin logoSize" src={GitHub} alt='GitHub logo used to send the user to my GitHub profile'></img></a>
+            <a target ='_blank' href="https://www.linkedin.com/in/ricky-medina-608634244/"><img className="setMargin logoSize" src={linkedIn} alt='LinkedIn logo used to send the user to my LinkedIn profile'></img></a>
+        </div>
+    );
+}
+
+export default Footer;
