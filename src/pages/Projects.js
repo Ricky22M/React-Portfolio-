accb22ff8c2f6e0f2cf576dde0bc51393f85177c
Ricky Esparza <RickyEsparza@Rickys-MacBook.local>
2023-02-05 20:34:56 -0700

    added new folder and images to the images folder, also started on projects.js and projects.css files

---
+++ src/pages/Projects.js
@@ -0,0 +1,27 @@
+// This will render the projects age
+
+// importing react
+import React from 'react';
+
+// importing images from my previous individual projects
+import socialNetWork from '../images/Projects/Social-network-api-project.png';
+import eCommerceBackend from '../images/Projects/E-commerce-Backedn-project.png';
+import employeeTracker from '../images/Projects/Employee-tracker-project.png';
+import teamProfileGen from '../images/Projects/Team-Profile-Generator-Screenshot.png';
+import weatherDashboard from '../images/Projects/Weather-Dashboard-Webpage.png';
+import workDayScheduler from '../images/Projects/Work-Day-Scheduler-project.png';
+import passwordGen from '../images/Projects/Password-Generator.png';
+import codeQuiz from '../images/Projects/Code-Quiz-project.png';
+
+// importing more images from my previous group projects
+import recipePicker from '../images/Projects/Recipe-Picker-project.png';
+import musicBox from '../images/Projects/My-Musicbox-project.png';
+
+// Returns projects page
+function Projects() {
+    return (
+        
+    )
+}
+
+export default Projects;
